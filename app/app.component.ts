8b8410eea02de5dd9bd77d751c18f04059ef185b
Ryan Johns <jedimasterjohns@gmail.com>
2018-08-15 09:09:39 -0600

    21 percent

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
+  <div class="app">
+    {{ title }}
+    <div>
+    {{ numberOne + numberTwo }}
     </div>
+  </div>
   `
 })
 export class AppComponent {
-
+  title: string;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
